79ea30c83721e4b69700e297f918242a35272100
Data_bird <83010684+jiyamaryjoseph@users.noreply.github.com>
2023-06-15 22:05:33 +0530

    change some of text and images

--- Web_Design/CoffeeDay/js/custom.js
+++ Web_Design/CoffeeDay/js/custom.js
@@ -10,17 +10,12 @@ function openNav() {
     document.getElementById("myNav").classList.toggle("menu_width");
     document.querySelector(".custom_menu-btn").classList.toggle("menu_btn-style");
 }
-/** google_map js **/
 
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
-        zoom: 18,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
-// lightbox gallery
+
+
 $(document).on("click", '[data-toggle="lightbox"]', function (event) {
     event.preventDefault();
     $(this).ekkoLightbox();
 });
+
+
